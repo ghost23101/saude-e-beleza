8061de23c718d412955b2841263f652911d42b8e
Isaque A.P. <126116466+ghost23101@users.noreply.github.com>
2024-12-20 14:00:14 -0300

    colocando os araquivos do site

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// Efeitos de ScrollReveal
+window.sr = ScrollReveal({reset:true});
+
+sr.reveal('.grid-conteudo', {duration: 3000});
+sr.reveal('.video', {duration: 6000});
+sr.reveal('.grid-equipe', {
+    duration: 6000,
+    rotate: {x: 0, y: 100, z: 5},
+    //reset: true
+});
